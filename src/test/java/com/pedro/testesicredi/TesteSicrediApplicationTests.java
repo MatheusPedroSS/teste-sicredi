270127b1bcf2eafbd1d651ee78549105aa856977
Matheus Pedro <matheus.pedro1214@gmail.com>
2021-05-17 19:03:25 -0300

    Commit Inicial

---
+++ src/test/java/com/pedro/testesicredi/TesteSicrediApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedro.testesicredi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteSicrediApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
